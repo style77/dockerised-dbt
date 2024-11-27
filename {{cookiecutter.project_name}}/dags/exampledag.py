a714c62fbb05e6fc38c33a7bdf70cb9cb5f525ca
style77 <stylek777@gmail.com>
2024-11-27 20:32:55 +0100

    fix: postgres connection

--- {{cookiecutter.project_name}}/dags/exampledag.py
+++ {{cookiecutter.project_name}}/dags/exampledag.py
@@ -1,17 +1,17 @@
 from airflow.decorators import dag
-from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig
+from cosmos import DbtTaskGroup, ExecutionConfig, ProjectConfig, ProfileConfig
 from pendulum import datetime
 import os
 import sys
 
 # This is mandatory, as AIRFLOW runs the DAGs from `tmp` directory
-airflow_home = os.environ['AIRFLOW_HOME']
-sys.path.append(airflow_home)
+AIRFLOW_PATH = "/usr/local/airflow"
+sys.path.append(AIRFLOW_PATH)
 
 profile_config = ProfileConfig( 
-    profile_name="{{ cookiecutter.project_name }}", 
-    target_name="{{ cookiecutter.project_name }}", 
-    profiles_yml_filepath=f"../{{cookiecutter.dbt_project_name}}/.dbt/profiles.yml"
+    profile_name="{{ cookiecutter.dbt_project_name }}", 
+    target_name="{{ cookiecutter.profile_target }}", 
+    profiles_yml_filepath=f"{AIRFLOW_PATH}/dbt/.dbt/.profiles.yml"
 )
 
 default_args = {
@@ -30,12 +30,19 @@ default_args = {
 def example_pipeline():
     transform_data = DbtTaskGroup(
         group_id="transform_data",
-        project_config=ProjectConfig(DBT_PROJECT_DIR,),
+        project_config=ProjectConfig(
+            dbt_project_path=f"{AIRFLOW_PATH}/dbt",
+        ),
         profile_config=profile_config,
+        execution_config=ExecutionConfig(
+            dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
+        ),
         operator_args={ 
-            "install_deps": True, 
+            "install_deps": True,  # required to install dbt dependencies
         }, 
         default_args=default_args,
     )
+
+    transform_data
 
 example_pipeline()
